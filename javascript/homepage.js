0c0123f9fd0b24a294aa55c163a1ce0b2f96e8e4
Alex <xcualidoxkuruzu@gmail.com>
2024-04-17 21:17:47 -0400

    Implementado diseño del login en el homepage

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,6 +1,10 @@
 const slider = document.getElementById("slider");
 const btnSliderRight = document.getElementById("btnSliderRight");
 const btnSliderLeft = document.getElementById("btnSliderLeft");
+const openLoginBtn = document.getElementById("openLogin");
+const closeLoginBtn = document.getElementById("closeLogin");
+const loginModal = document.getElementById("loginModal");
+const loginForm = document.getElementById("loginForm");
 let sliderSection = document.querySelectorAll("[data-slider]");
 let sliderSectionLast = sliderSection[sliderSection.length - 1];
 
@@ -31,6 +35,28 @@ function sliderMoveLeft()
     }, 500);
 
 }
+function showLogin()
+{
+    loginModal.classList.remove("hidden");
+    loginModal.classList.add("flex");
+    setTimeout(()=>
+    {
+        loginModal.classList.add("opacity-100");
+    },50);
+
+}
+function hideLogin()
+{
+    
+    loginModal.classList.add("opacity-0");
+    loginModal.classList.remove("opacity-100");
+    setTimeout(()=>
+    {
+        loginModal.classList.remove("flex");
+        loginModal.classList.add("hidden");
+    },500);
+
+}
 btnSliderRight.addEventListener("click",()=>
 {
     sliderMoveRight();
@@ -42,3 +68,20 @@ btnSliderLeft.addEventListener("click",()=>
 setInterval(() => {
     sliderMoveRight();
 }, 5000);
+
+openLoginBtn.addEventListener("click", ()=>
+{
+    showLogin();
+});
+closeLoginBtn.addEventListener("click", ()=>
+{
+    hideLogin();
+});
+loginModal.addEventListener("click", ()=>
+{
+    hideLogin();
+});
+loginForm.addEventListener("click", e=>
+{
+    e.stopImmediatePropagation();
+});
