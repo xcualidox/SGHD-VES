2a3b5c17e3970abb489e4aa716a62b87e0b9aca6
Rafael Molina <zuhe2010@gmail.com>
2024-08-15 16:49:38 -0400

    Hotfix
    el CRUD de profesor ya funciona

--- Angel_Guarda/Modelo/profesor.php
+++ Angel_Guarda/Modelo/profesor.php
@@ -3,27 +3,25 @@ include_once("basedatos.php");
 class zona extends database_connect{
     private $nom, $fecI, $fechaF;
 
-    function setDatos($cedula, $p_nombre, $s_nombre, $p_apellido, $s_apellido, $direccion, $telefono, $correo){
+    function setDatos($cedula, $nombres, $apellidos, $direccion, $telefono, $correo){
 		$this->cedula=$cedula;
-		$this->p_nombre=strtoupper($p_nombre);
-    $this->s_nombre=strtoupper($s_nombre);
-    $this->p_apellido=strtoupper($p_apellido);
-    $this->s_apellido=strtoupper($s_apellido);
+		$this->nombres=strtoupper($nombres);
+    $this->apellidos=strtoupper($apellidos);
     $this->direccion=strtoupper($direccion);
     $this->telefono=$telefono;
     $this->correo=$correo;
 	  }
 
     function incluye(){
-        $sql= "insert into personas(cedula, nombres, apellidos, direccion, telefono, correo) values(?,?,?,?,?,?,?,?)";
-		return $this->query($sql,[$this->cedula,$this->p_nombre,$this->s_nombre,$this->p_apellido,$this->s_apellido,$this->direccion,$this->telefono,$this->correo]);
+        $sql= "insert into personas(cedula, nombres, apellidos, direccion, telefono, correo) values(?,?,?,?,?,?)";
+		return $this->query($sql,[$this->cedula,$this->nombres,$this->apellidos,$this->direccion,$this->telefono,$this->correo]);
     }
 
     function modificar($origin){
         $sql= "UPDATE `personas`
                 SET `cedula`=?, `nombres`=?, `apellidos`=?, `direccion`=?, `telefono`=?, `correo`=?
                 WHERE `cedula`=?";
-		return $this->query($sql,[$this->cedula,$this->p_nombre,$this->s_nombre,$this->p_apellido,$this->s_apellido,$this->direccion,$this->telefono,$this->correo,$origin]);
+		return $this->query($sql,[$this->cedula,$this->nombres,$this->apellidos,$this->direccion,$this->telefono,$this->correo,$origin]);
     }
     
     function eliminar($origin) {
