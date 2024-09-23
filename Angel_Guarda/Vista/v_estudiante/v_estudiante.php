f87fe0be5fac2c542d5f75a639316ec3eece9ef7
Angel Mendez <110472083+Enyel04@users.noreply.github.com>
2024-09-23 13:15:59 -0400

    Agregando Radio de input

--- Angel_Guarda/Vista/v_estudiante/v_estudiante.php
+++ Angel_Guarda/Vista/v_estudiante/v_estudiante.php
@@ -298,145 +298,206 @@ include_once('../v_Sidebar/v_Sidebar.php');
 
     <dialog id="modalPagosEspecificos">
 
-    <div class="flex justify-between">
-    <!-- Título y descripción -->
-        <div class="flex flex-col">
-            <h1 class="text-xl font-semibold">Pago de Estudiante</h1>
-            <h2 class=" font-semibold">"Nombre Nombre y Apellido Apellido"</h2>
-            <h3 class=" font-semibold">Cedula Estudiante: 565656</h3>
-            <h3 class=" font-semibold">Cedula Del Representante: 565656</h3>
-         
-        </div>
+        <div class="flex justify-between">
+            <!-- Título y descripción -->
+            <div class="flex flex-col">
+                <h1 class="text-xl font-semibold">Pago de Estudiante</h1>
+                <h2 class=" font-semibold">"Nombre Nombre y Apellido Apellido"</h2>
+                <h3 class=" font-semibold">Cedula Estudiante: 565656</h3>
+                <h3 class=" font-semibold">Cedula Del Representante: 565656</h3>
 
-    <!-- Botón de cerrar en la parte superior derecha -->
-    <div class="w-10 h-10 bg-red-500 rounded-full cursor-pointer p-2" id="closePagosEspecificos">
-    <img src="../../../images/icons/error.svg" class="filtro-blanco w-full h-full" alt="Cerrar" title="Cerrar">
-</div>
-</div>
+            </div>
+
+            <!-- Botón de cerrar en la parte superior derecha -->
+            <div class="w-10 h-10 bg-red-500 rounded-full cursor-pointer p-2" id="closePagosEspecificos">
+                <img src="../../../images/icons/error.svg" class="filtro-blanco w-full h-full" alt="Cerrar" title="Cerrar">
+            </div>
+        </div>
         <details>
             <summary>Registrar Pago</summary>
             <div class="content__details">
-      
-
-<form id="" class="space-y-4" method="POST" action="#">
-    <!-- Contenedor principal con flex para dividir izquierda y derecha -->
-    <div class="flex space-x-8">
-        <!-- Columna Izquierda -->
-        <div class=" w-full">
-            <div class="formulario-extenso__row">
-                <div class="formulario-extenso__column">
-                    <label for="mesPagar">Meses a Pagar: </label>
-                    <input type="text" id="mesPagar" name="mesPagar" placeholder="Meses a pagar" class="formulario-extenso__input" maxlength="25">
-                </div>
-            </div>
-            <div class="formulario-extenso__row">
-                <div class="formulario-extenso__column">
-                    <label for="Pago">Forma de Pago: </label>
-                   
-                    <input type="radio" id="efectivo" value="divisas" name="tipoPago">Divisas
-                    <input type="radio" id="" name="tipoPago">Transferencia
-                </div>
-            
-            </div>
-            <div class="formulario-extenso__row">
-                <div class="formulario-extenso__column">
-                    <label for="anoEscolar">Año Escolar: </label>
-                    <select name="" id="" class="formulario-extenso__input">
-                        <option value="">Año Escolar 1</option>
-                        <option value="">Año Escolar 2</option>
-                    </select>
-                </div>
-                <div class="formulario-extenso__column">
-                <div class="formulario-extenso__column">
-                    <label for="monto">Monto a Pagar: </label>
-                    <input type="text" id="mesPagar" name="mesPagar" placeholder="Monto a Pagar" class="formulario-extenso__input" maxlength="25">
-                </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Columna Derecha -->
-       
 
 
-    </div>
+                <form id="" class="space-y-4" method="POST" action="#">
+                    <!-- Contenedor principal con flex para dividir izquierda y derecha -->
+                    <div class="flex space-x-8">
+                        <!-- Columna Izquierda -->
+                        <div class=" w-full">
+                            <div class="formulario-extenso__row">
+                                <div class="formulario-extenso__column">
+                                    <label for="anoEscolar">Año Escolar: </label>
+                                    <select name="" id="" class="formulario-extenso__input">
+                                        <option value="">Año Escolar 1</option>
+                                        <option value="">Año Escolar 2</option>
+                                    </select>
+                                </div>
+                                <div class="formulario-extenso__column">
+                                    <label for="mesPagar">Meses a Pagar: </label>
+                                    <input type="text" id="mesPagar" name="mesPagar" placeholder="Meses a pagar" class="formulario-extenso__input" maxlength="25">
+                                </div>
+                            </div>
+
+                            <div class="formulario-extenso__row">
+                                <div class="formulario-extenso__column">
+
+                                    <label for="Pago">Descuento: </label>
+                                    <div class="flex">
+                                    <div class="custom-radio">
+                                        <input type="radio" id="total" value="0" name="descuento">
+                                        <label for="total">Total</label>
+                                    </div>
+                                    <div class="custom-radio">
+                                        <input type="radio" id="descuento-10" value="10%" name="descuento">
+                                        <label for="descuento-10">10%</label>
+                                    </div>
+
+                                    <div class="custom-radio">
+                                        <input type="radio" id="descuento-20" value="20%" name="descuento">
+                                        <label for="descuento-20">20%</label>
+                                    </div>
+
+                                    <div class="custom-radio">
+                                        <input type="radio" id="descuento-50" value="50%" name="descuento">
+                                        <label for="descuento-50">50%</label>
+                                    </div>
+
+                                    <div class="custom-radio">
+                                        <input type="radio" id="descuento-becado" value="Becado" name="descuento">
+                                        <label for="descuento-becado">Becado</label>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="formulario-extenso__row">
+                                <div class="formulario-extenso__column">
+                                    <label for="Pago">Forma de Pago: </label>
+                                    <div class="flex">
+                                    <div class="custom-radio">
+                                        <input type="radio" id="divisas" value="divisas" name="FormaPago">
+                                        <label for="divisas">Divisas</label>
+                                    </div>
+                                    <div class="custom-radio">
+                                        <input type="radio" id="transferencia" value="transferencia" name="FormaPago">
+                                        <label for="transferencia">Transferencia</label>
+                                    </div>
+                                    </div>
+                                </div>
+                                <div class="formulario-extenso__column">
+
+                                    <label for="referencia">Numero de Referencia: </label>
+                                    <input type="text" id="referencia" name="referencia" placeholder="Referencia" class="formulario-extenso__input" maxlength="">
+
+                                </div>
+                            </div>
+                            <div class="formulario-extenso__row">
+
+                                <div class="formulario-extenso__column">
+
+                                    <label for="monto">Monto a Pagar: </label>
+                                    <input type="text" id="mesPagar" name="mesPagar" placeholder="Monto a Pagar" class="formulario-extenso__input" maxlength="">
+
+                                </div>
+                                <div class="formulario-extenso__column">
+
+                                    <label for="monto">Descuento: </label>
+                                    <input type="text" id="mesPagar" name="mesPagar" disabled placeholder="Descuento" class="formulario-extenso__input" maxlength="">
+
+                                </div>
+                            </div>
+                            <div class="formulario-extenso__row">
+
+                                <div class="formulario-extenso__column">
+
+                                    <label for="monto">Monto Total: </label>
+                                    <input type="text" id="mesPagar" name="mesPagar" placeholder="Monto a Pagar" class="formulario-extenso__input" maxlength="">
+
+                                </div>
+                                
+                            </div>
+                        </div>
+
+                        <!-- Columna Derecha -->
 
 
-    <input type="button" id="" value="Guardar" class="table_button w-full">
-</form>
+
+                    </div>
+
+
+                    <input type="button" id="" value="Guardar" class="table_button w-full">
+                </form>
             </div>
         </details>
 
         <details>
             <summary>Lista de Pagos</summary>
             <div class="content__details">
-            <div class="flex flex-col sm:flex-row justify-end items-center md:space-x-2 p-4 md:py-2">
+                <div class="flex flex-col sm:flex-row justify-end items-center md:space-x-2 p-4 md:py-2">
 
-                <div class="bg-gray-100 rounded-full cursor-pointer" id="openAñadirPago">
-                    <img src="../../../images/icons/añadir.svg" class="w-10 filtro-verde" alt="openAñadirPago" title="openAñadirPago">
+                    <div class="bg-gray-100 rounded-full cursor-pointer" id="openAñadirPago">
+                        <img src="../../../images/icons/añadir.svg" class="w-10 filtro-verde" alt="openAñadirPago" title="openAñadirPago">
+                    </div>
+                    <!-- FORMULARIO BUSQUEDA-->
+                    <form method="GET" action="" class="flex items-center space-x-2">
+                        <select name="campo" class='capitalize border-solid border-2  border-black' id="selectListar" name="listar" p class="border rounded px-2 py-1 w-auto">
+                        </select>
+
+                        <input type="text" id="listar" name="listar" placeholder="Buscar..." class="border rounded px-2 py-1 mb-2 md:mb-0">
+
+                        <button type="button" class="bg-gray-100 rounded-full">
+                            <img src="../../../images/icons/buscar.svg" class=" w-20 filtro-verde" alt="Buscar" title="Buscar">
+                        </button>
+                    </form>
+
+                    <!-- Botón para limpiar los filtros -->
+
                 </div>
-                <!-- FORMULARIO BUSQUEDA-->
-                <form method="GET" action="" class="flex items-center space-x-2">
-                    <select name="campo" class='capitalize border-solid border-2  border-black' id="selectListar" name="listar" p class="border rounded px-2 py-1 w-auto">
-                    </select>
+                <div class="table-wrapper min-w-full">
+                    <table class="fl-table">
+                        <thead>
+                            <tr>
+                                <td>ID</td>
+                                <td>Forma de Pago</td>
+                                <td>Total</td>
+                                <td>fecha</td>
+                                <td>Descuento</td>
+                                <td class='no_style'>
+                                    <div class="flex justify-center ">
 
-                    <input type="text" id="listar" name="listar" placeholder="Buscar..." class="border rounded px-2 py-1 mb-2 md:mb-0">
+                                        <a href="#">
+                                            <img src="../../../images/icons/pdf.svg" class="w-10  filtro-blanco " alt="Reporte" title="Reporte">
+                                        </a>
 
-                    <button type="button" class="bg-gray-100 rounded-full">
-                        <img src="../../../images/icons/buscar.svg" class=" w-20 filtro-verde" alt="Buscar" title="Buscar">
-                    </button>
-                </form>
+                                    </div>
+                                </td>
 
-                <!-- Botón para limpiar los filtros -->
-
-            </div>
-            <div class="table-wrapper min-w-full">
-                <table class="fl-table">
-                    <thead>
-                        <tr>
-                            <td>ID</td>
-                            <td>Forma de Pago</td>
-                            <td>Total</td>
-                            <td>fecha</td>
-                            <td>Descuento</td>
-                            <td class='no_style'>
-                                <div class="flex justify-center ">
-
-                                    <a href="#">
-                                        <img src="../../../images/icons/pdf.svg" class="w-10  filtro-blanco " alt="Reporte" title="Reporte">
-                                    </a>
-
-                                </div>
-                            </td>
-
-                        </tr>
-                    </thead>
-                    <tbody>
+                            </tr>
+                        </thead>
+                        <tbody>
 
 
 
-                        <tr>
-                            <td class="border px-4 py-2"> Año Escolar</td>
-                            <td class="border px-4 py-2">Nombre y Apellido </td>
-                            <td class="border px-4 py-2">Año Sección</td>
-                            <td class="border px-4 py-2">Telefono Representante</td>
+                            <tr>
+                                <td class="border px-4 py-2"> Año Escolar</td>
+                                <td class="border px-4 py-2">Nombre y Apellido </td>
+                                <td class="border px-4 py-2">Año Sección</td>
+                                <td class="border px-4 py-2">Telefono Representante</td>
 
-                            <td class="border px-4 py-2 text-center">
-                                <div class="flex justify-center items-center space-x-4">
-                                    <img src="../../../images/icons/pdf.svg" class="w-8 h-8 filtro-rojo cursor-pointer" alt="PDF" title="PDF">
-                                    <img src="../../../images/icons/modificar.svg" class="w-8 h-8 filtro-azul cursor-pointer" alt="Modificar" title="Modificar">
+                                <td class="border px-4 py-2 text-center">
+                                    <div class="flex justify-center items-center space-x-4">
+                                        <img src="../../../images/icons/pdf.svg" class="w-8 h-8 filtro-rojo cursor-pointer" alt="PDF" title="PDF">
+                                        <img src="../../../images/icons/modificar.svg" class="w-8 h-8 filtro-azul cursor-pointer" alt="Modificar" title="Modificar">
 
-                                </div>
-                            </td>
-                            <td class="border px-4 py-2">1/12</td>
-                        </tr>
+                                    </div>
+                                </td>
+                                <td class="border px-4 py-2">1/12</td>
+                            </tr>
 
 
-                    </tbody>
-                </table>
-                <!-- Mostrando El total de Paginas -->
+                        </tbody>
+                    </table>
+                    <!-- Mostrando El total de Paginas -->
 
-            </div>
+                </div>
             </div>
         </details>
 
