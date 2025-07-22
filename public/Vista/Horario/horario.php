ffa40e45b4adc7032dd20b651299709e1a6c03e6
Angel Mendez <110472083+Enyel04@users.noreply.github.com>
2025-07-21 21:57:23 -0400

    fixed reporte

--- public/Vista/Horario/horario.php
+++ public/Vista/Horario/horario.php
@@ -102,11 +102,10 @@ while ($mostrar = mysqli_fetch_array($intervalo)) {
 
 
 
-                                <a href='horario_pdf.php?codigo_escolar=<?php echo $mostrar["ano_escolar"];
-                                                                        ?>&codigo_seccion=<?php echo $mostrar["codigo_a_y_seccion"];
-                        ?>&nombre=<?php echo $mostrar["nombre"]; ?>&ano=<?php echo $mostrar["ano"];
-                                                    ?>&seccion=<?php echo $mostrar["seccion"]; ?>&intervalo=<?php echo $mostrar["intervalo"]; ?>&receso=<?php echo $mostrar["receso"]; ?>' target="_blank">
-                                    <img src="../../../images/icons/pdf.svg" class="w-10 filtro-verde">
+                                                  <a href="../pdf/horarioDocentePDF.php?cedula=<?= $mostrar['cedula'] ?>&anoEscolar=<?= $mostrar['ano_codigo'] ?>" target="_blank">
+                                                            <img src="../../../images/icons/pdf.svg" class="w-10 filtro-verde" alt="PDF Horario">
+                                                        </a>
+
                                 </a>
                             </div>
 
